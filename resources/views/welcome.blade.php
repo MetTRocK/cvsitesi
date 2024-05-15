4f3839835ae0a5f886072c35814c4a34ec9dbad6
MetTRocK <mettrock1@gmail.com>
2024-05-15 21:23:39 +0300

    21:23

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Deneme</title>
+        <title>Deneme12</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
